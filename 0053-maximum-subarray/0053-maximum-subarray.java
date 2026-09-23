3db4244fe550f9307c308e29b46ae711a6634bf2
MOHAMMAD ASAD SAIFI <asad01qadri@gmail.com>
2026-09-24 04:43:07 +0530

    Time: 1 ms (99.92%), Space: 77.2 MB (48.99%) - LeetHub

---
+++ 0053-maximum-subarray/0053-maximum-subarray.java
@@ -0,0 +1,12 @@
+class Solution {
+    public int maxSubArray(int[] arr ){
+        int currentSum=arr[0];
+        int maxSum=arr[0];
+        for(int i =1;i<arr.length;i++){
+            currentSum=Math.max(arr[i],currentSum+arr[i]);
+            maxSum=Math.max(maxSum,currentSum);
+        }
+        return maxSum;
+
+    }
+}
